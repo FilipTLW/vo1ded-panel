3fabccbb55836a6779ce3b690214c26c5720bda4
Filip Myslinski <filipmyslinski2006@gmail.com>
2025-06-20 23:07:02 +0200

    feat: confirmation dialog for power options

--- widget/Settings.tsx
+++ widget/Settings.tsx
@@ -1,6 +1,8 @@
 import {App, Gdk} from "astal/gtk3";
 import Astal from "gi://Astal?version=3.0";
 import {Variable} from "astal";
+import {exec} from "astal/process";
+import {ConfirmationDialog} from "./ConfirmationDialog";
 
 const {WindowAnchor} = Astal;
 
@@ -41,7 +43,8 @@ export function SettingsMenu() {
           <box
           >
             <button
-              onClickRelease='systemctl poweroff'
+              // onClickRelease='systemctl poweroff'
+              onClickRelease={() => ConfirmationDialog('Are you sure?', 'Do you want to shut down now?', () => exec('systemctl poweroff'))}
               width_request={128}
             >
               Shutdown
@@ -51,7 +54,7 @@ export function SettingsMenu() {
             className='zero-margin zero-padding'
           >
             <button
-              onClickRelease='systemctl reboot'
+              onClickRelease={() => ConfirmationDialog('Are you sure?', 'Do you want to reboot now?', () => exec('systemctl reboot'))}
               width_request={128}
             >
               Reboot
@@ -60,7 +63,7 @@ export function SettingsMenu() {
           <box
           >
             <button
-              onClickRelease='hyprctl dispatch exit 1'
+              onClickRelease={() => ConfirmationDialog('Are you sure?', 'Do you want to log out now?', () => exec('hyprctl dispatch exit 1'))}
               width_request={128}
             >
               Logout
